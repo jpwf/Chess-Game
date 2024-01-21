bd7db1d7e56304c0f85af9109a4fc236c77c973d
jpwf <joaopedroweydt@gmail.com>
2024-01-21 19:14:46 -0300

    pieces created and styles started

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ const infoDisplay = document.querySelector("#info-display")
 
 const width = 8;
 
-const S_pieces = {}
+const S_pieces = [
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  "","","","","","","","",
+  "","","","","","","","",
+  "","","","","","","","",
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook
+]
+
+function createBoard(){
+  S_pieces.forEach((s_piece)=>{
+    const square = document.createElement('div')
+    square.classList.add('square')
+  })
+}
